257c9127c06b20d365242f7fb95c7a4bbca4671d
anushkashukla13 <anushkashukla1331@gmail.com>
2025-04-08 10:42:13 +0530

    hello

---
+++ hello/src/new1.java
@@ -0,0 +1,16 @@
+public class new1 {
+    public static void main(String[] args)
+    {
+        new1 n  =new new1();
+        n.show();
+        n.sum(10,40);
+    }
+    public void show()
+    {
+        System.out.println("hello bhai>...");
+    }
+    public void sum(int a , int b)
+    {
+        System.out.println(a+b);
+    }
+}

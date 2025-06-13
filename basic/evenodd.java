7fc16e4b47cddb6b2ba4ff1d6796744c1559ec80
anushkashukla13 <anushkashukla1331@gmail.com>
2025-06-13 11:12:50 +0530

    fifth

---
+++ basic/evenodd.java
@@ -0,0 +1,52 @@
+package basic;
+
+public class evenodd {
+        //1. check wheather the number is even or odd 
+        // public static void main(String[] args) {
+        //     int a=9;
+        //     if(a==0){
+        //         System.out.println("the number is zero");
+        //     }
+        //     else if(a%2==0){
+        //         System.out.println("the number is even");
+        //     }
+        //     else{
+        //         System.out.println("the number is odd");
+        //     }
+        // }
+        // 2. check positive , negative or zero 
+        // public static void main(String[] args) {
+        //     int a=-2;
+        //     if(a>0){
+        //         System.out.println("positive");
+        //     }
+        //     else{
+        //         System.err.println("negative");
+        //     }
+        // }
+
+        // 3. find the greatest between three numbers
+        // public static void main(String[] args) {
+        //     int a=120,b=16,c=311;
+        //     if(a>b && a>c){
+        //         System.out.println("a is greater");
+        //     }
+        //     else if(b>a && b>c){
+        //         System.out.println("b is greater");
+        //     }
+        //     else System.out.println("c is greater");
+        // }
+
+        // 4.check wheather the alphabet is vowel or consonent.
+//         public static void main(String[] args) {
+            
+//         char ch='t';
+//         if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' )
+//         {
+//             System.err.println("i am vowel");
+//         }
+//         else{
+//             System.out.println("i am consonat");
+//         }
+//         }
+} 
---
+++ ccccc/t.java
@@ -0,0 +1,12 @@
+package ccccc;
+import java.util.Scanner;
+
+public class t {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter number  :");
+        int n = in.nextInt();
+
+        System.out.println(n);
+    }
+}
